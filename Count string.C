090d82723bfab6ffd7edfa3add4c04a7e3b313b4
Subro sutradhar <subroopu@gmail.com>
2025-08-08 22:48:19 +0600

    Create Count string.C

---
+++ Count string.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char S[1000001];  
+    int sum = 0;
+
+    
+    scanf("%s", S);
+
+  
+    for (int i = 0; S[i] != '\0'; i++) {
+        sum += S[i] - '0';  
+    }
+
+  
+    printf("%d\n", sum);
+
+    return 0;
+}
